a05262deec2d353989888e5da1a5f50c4e05aa96
nomoreFt <hyunwoo.kim1108@gmail.com>
2021-07-01 18:47:00 +0900

    Initial commit

---
+++ src/main/java/readinglist/Book.java
@@ -0,0 +1,27 @@
+package readinglist;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Book {
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String reader;
+	private String isbn;
+	private String title;
+	private String author;
+	private String description;
+}
+
---
+++ src/test/java/readinglist/ReadingListApplicationTests.java
@@ -0,0 +1,13 @@
+package readinglist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = ReadingListApplication.class)
+class ReadingListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
